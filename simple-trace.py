84e036bedfc600c7593f60c2897116ffad0a8614
James Loser <james.loser@gmail.com>
2012-02-09 17:26:24 -0500

    cleaned up, removed DNS queries

--- simple-trace.py
+++ simple-trace.py
@@ -15,13 +15,13 @@ def create_sockets(ttl):
   recv_socket.setblocking(0)
   return recv_socket, send_socket
 
-def main(dest_name, port, max_hops):
+def main(dest_name, max_hops):
     dest_addr = socket.gethostbyname(dest_name)
     timeout_in_seconds = 1
     ttl = 1
     while True:
         recv_socket, send_socket = create_sockets(ttl)
-        send_socket.sendto("", (dest_name, port))
+        send_socket.sendto("", (dest_name, 33434))
         curr_addr = None
         curr_name = None
         try:
@@ -31,10 +31,6 @@ def main(dest_name, port, max_hops):
                 return
             _, curr_addr = recv_socket.recvfrom(2048)
             curr_addr = curr_addr[0]
-            try:
-                curr_name = socket.gethostbyaddr(curr_addr)[0]
-            except socket.error:
-                curr_name = curr_addr
         except socket.error:
             pass
         finally:
@@ -55,18 +51,14 @@ def main(dest_name, port, max_hops):
 
 if __name__ == "__main__":
   parser = optparse.OptionParser(usage="%prog [options] hostname")
-  parser.add_option("-p", "--port", dest="port",
-                    help="Port to use for socket connection [default: %default]",
-                    default=33434, metavar="PORT")
   parser.add_option("-m", "--max-hops", dest="max_hops",
                     help="Max hops before giving up [default: %default]",
                     default=30, metavar="MAXHOPS")
   options, args = parser.parse_args()
   if len(args) != 1:
-    print 'usage: traceroute.py [options] hostname'
+    print 'usage: simple-trace.py [options] hostname'
     exit(0)
   else:
     dest_name = args[0]
     sys.exit(main(dest_name=dest_name,
-             port=int(options.port),
              max_hops=int(options.max_hops)))
